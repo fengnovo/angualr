4362ff7c8ddc7762346698b3d511a310c6dfa619
huangyunfeng <283563375@qq.com>
2017-07-11 14:15:32 +0800

    a contacts demo

--- angular2/demo/ngDemo01/src/app/app.module.ts
+++ angular2/demo/ngDemo01/src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {NgModule} from '@angular/core'
+import {RouterModule} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpModule} from "@angular/http";
 
-import { AppComponent } from './app.component';
+import {rootRouterConfig} from "./app.routes";
+import {AppComponent} from "./app.component";
+
+import {CollectionComponent} from './collection';
+import {ListComponent, ListItemComponent} from './list';
+import {DetailComponent} from './detail';
+import {EditComponent} from './edit';
+
+import {ContactService, UtilService, FooterComponent, 
+  HeaderComponent, PhonePipe, BtnClickDirective} from "./shared";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent, ListItemComponent,
+    DetailComponent,
+    CollectionComponent,
+    EditComponent,
+    HeaderComponent, FooterComponent,
+    PhonePipe,
+    BtnClickDirective
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
+  providers   : [ContactService, UtilService],
+  bootstrap   : [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
